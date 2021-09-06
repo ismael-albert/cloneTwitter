43d3621e0cefb4ef7ee39b5ed680c5d3e0de9830
Ismael Albert <62386498+ismael-albert@users.noreply.github.com>
2021-09-06 12:23:23 -0300

    Update index.tsx

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -23,8 +23,8 @@ const Main: React.FC = () => {
         </button>
 
         <ProfileInfo>
-          <strong>Guilherme Rodz</strong>
-          <span>612 Tweets</span>
+          <strong>Ismael Albert</strong>
+          <span>999 Tweets</span>
         </ProfileInfo>
       </Header>
 
